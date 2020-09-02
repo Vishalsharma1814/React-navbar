c0e2d9d4c8d801f9cdb75d9bcf240986a79822c6
Vishalsharma1814 <65768731+Vishalsharma1814@users.noreply.github.com>
2020-09-02 22:59:39 +0530

    Updates

---
+++ precache-manifest.564e12bdd2c79971aeabf443f0c140a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6522cde4a4167dd84bf1166fac6a4d69",
+    "url": "/React-navbar/index.html"
+  },
+  {
+    "revision": "7467de5fdbe1c706811c",
+    "url": "/React-navbar/static/css/main.09f2e66c.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/React-navbar/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-navbar/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7467de5fdbe1c706811c",
+    "url": "/React-navbar/static/js/main.3a6fc845.chunk.js"
+  },
+  {
+    "revision": "344006cd8fea41d02f41",
+    "url": "/React-navbar/static/js/runtime-main.51315684.js"
+  }
+]);
